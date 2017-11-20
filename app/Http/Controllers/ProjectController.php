b9ce1c7883b3cdcda6ef57a4678da0ab46c69beb
Gopal Sharma <gopalindians@gmail.com>
2017-11-20 17:13:23 +0530

    updated for hadling Create project data

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,44 +3,49 @@
 namespace App\Http\Controllers;
 
 use Auth, DB;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-class ProjectController extends Controller
-{
-    //
+class ProjectController extends Controller {
+	//
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+	public function __construct() {
+		$this->middleware( 'auth' );
+	}
 
-    public function index():View
-    {
-        $id = Auth::id();
-        $projects = DB::table('projects')
-            ->join('users', 'users.id', '=', 'projects.owner_id')
-            ->where('owner_id', '=', $id)
-            ->get();
-        dump($projects);
-        return view('project.index', ['projects' => $projects]);
-    }
+	public function index(): View {
+		$id       = Auth::id();
+		$projects = DB::table( 'projects' )
+		              ->join( 'users', 'users.id', '=', 'projects.owner_id' )
+		              ->where( 'owner_id', '=', $id )
+		              ->get();
+		dump( $projects );
 
-    public function createProject():View
-    {
-        return view('project.create_new');
-    }
+		return view( 'project.index', [ 'projects' => $projects ] );
+	}
 
-    public function postCreateProject(Request $request)
-    {
-        $validatedData = $request->validate([
-            'project_name' => 'required|max:255',
-            'project_description' => 'required',
-        ]);
+	public function createProject(): View {
+		return view( 'project.create_new' );
+	}
 
-        //DB::table('projects')->;
+	public function postCreateProject( Request $request ) {
+		$validatedData = $request->validate( [
+			'project_name'        => 'required|max:255',
+			'project_description' => 'required',
+		] );
 
-        return view('project.create_new');
+		DB::table( 'projects' )->insert( [
+				'owner_id'            => Auth::id(),
+				'project_name'        => $request->post( 'project_name' ),
+				'project_description' => $request->post( 'project_description' ),
+				'state'               => 'active',
+				'created_at'           => Carbon::now(),
+				'updated_at'           => Carbon::now()
+			]
+		);
 
-    }
+		return view( 'project.create_new' );
+
+	}
 }
