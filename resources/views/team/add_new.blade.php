0f3c954d2e83a30598a863941519ee517a038b6f
Gopal Sharma <gopalindians@gmail.com>
2017-11-20 19:01:58 +0530

    added form for adding new member

---
+++ resources/views/team/add_new.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('title',  '('.count($teams).') Your Team')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-danger">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @if(count($teams)==0)
+
+                        <div class="alert alert-danger">
+                            You dont have any team memebers yet
+                            add new <a href="/addNewTeamMember">Team</a>
+                        </div>
+                    @else
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Total Members - {{count($teams)}}</div>
+                            <div class="panel-body">
+                                <div class="row">
+                                    @foreach($teams as $team)
+
+                                        <div class="col-sm-6 col-md-4">
+                                            <div class="thumbnail">
+                                                <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTM0IiBoZWlnaHQ9IjE4MCIgdmlld0JveD0iMCAwIDEzNCAxODAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzEwMCV4MTgwCkNyZWF0ZWQgd2l0aCBIb2xkZXIuanMgMi42LjAuCkxlYXJuIG1vcmUgYXQgaHR0cDovL2hvbGRlcmpzLmNvbQooYykgMjAxMi0yMDE1IEl2YW4gTWFsb3BpbnNreSAtIGh0dHA6Ly9pbXNreS5jbwotLT48ZGVmcz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPjwhW0NEQVRBWyNob2xkZXJfMTVmYzlmMjdkNjYgdGV4dCB7IGZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0IH0gXV0+PC9zdHlsZT48L2RlZnM+PGcgaWQ9ImhvbGRlcl8xNWZjOWYyN2Q2NiI+PHJlY3Qgd2lkdGg9IjEzNCIgaGVpZ2h0PSIxODAiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSI0MC41IiB5PSI5NC44Ij4xMzR4MTgwPC90ZXh0PjwvZz48L2c+PC9zdmc+" alt="...">
+                                                <div class="caption">
+                                                    <h5>{{$team->member_name}}</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                </div>
+
+
+                @endif
+
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
