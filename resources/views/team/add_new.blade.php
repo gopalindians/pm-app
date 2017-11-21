14bf92b7ea5c5060982783d857cbbc6fc20afc3b
Gopal Sharma <gopalindians@gmail.com>
2017-11-21 18:51:20 +0530

    Modified view

--- resources/views/team/add_new.blade.php
+++ resources/views/team/add_new.blade.php
@@ -14,12 +14,29 @@
                         </div>
                     @endif
 
+                    @if (session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
 
                     <form action="/team/add" method="post">
                         {{csrf_field()}}
                         <div class="form-group">
                             <label for="user_email">Email</label>
-                            <input type="email" class="form-control" id="user_email" placeholder="Email">
+                            <input type="text" class="form-control" id="user_email" placeholder="Email"
+                                   name="user_email" value="{{old('user_email')}}">
                         </div>
                         <button type="submit" class="btn btn-default">Add</button>
                     </form>
