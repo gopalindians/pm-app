11bc3ff42a8001e15c7c3ed6c00ae90543894356
Gopal Sharma <gopalindians@gmail.com>
2017-11-23 19:08:31 +0530

    Added migartion to add more field to the users table

---
+++ database/migrations/2017_11_23_131556_add_dob_gender_position_profile_pic_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDobGenderPositionProfilePicToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('dob')->nullable()->describe('Date of birth');
+            $table->enum('gender', ['m', 'f', 't', 'nd'])->nullable()->describe('Gender: Male, Female, Trans gender,Not disclosed');
+            $table->text('position')->nullable()->describe('Position at which the user is working');
+            $table->text('profile_image')->nullable()->describe('Profile image of the user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['dob', 'gender', 'position','profile_image']);
+        });
+    }
+}
