c2d45639bc1f415c4a6edf30e93e2b4d2d56546a
Gopal Sharma <gopalindians@gmail.com>
2017-11-23 19:04:52 +0530

    Added edit profile blade tempate

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', $user[0]->name)
+@section('title', 'ff')
 
 @section('content')
     <div class="container">
@@ -16,22 +16,61 @@
                             </div>
                         @endif
 
-
-
-                            <div class="row">
-                                <div class="col-xs-6 col-md-3">
-                                    <a href="#" class="thumbnail">
-                                        <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTM0IiBoZWlnaHQ9IjE4MCIgdmlld0JveD0iMCAwIDEzNCAxODAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzEwMCV4MTgwCkNyZWF0ZWQgd2l0aCBIb2xkZXIuanMgMi42LjAuCkxlYXJuIG1vcmUgYXQgaHR0cDovL2hvbGRlcmpzLmNvbQooYykgMjAxMi0yMDE1IEl2YW4gTWFsb3BpbnNreSAtIGh0dHA6Ly9pbXNreS5jbwotLT48ZGVmcz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPjwhW0NEQVRBWyNob2xkZXJfMTVmYzlmMjdkNjYgdGV4dCB7IGZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0IH0gXV0+PC9zdHlsZT48L2RlZnM+PGcgaWQ9ImhvbGRlcl8xNWZjOWYyN2Q2NiI+PHJlY3Qgd2lkdGg9IjEzNCIgaGVpZ2h0PSIxODAiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSI0MC41IiB5PSI5NC44Ij4xMzR4MTgwPC90ZXh0PjwvZz48L2c+PC9zdmc+" alt="...">
-                                    </a>
+                        <form action="/profile/edit/{{$user[0]->id}}" method="post" enctype="multipart/form-data">
+                            {{csrf_field()}}
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="sizing-addon2">Name</span>
+                                    <input type="text" class="form-control" name="name" placeholder=""
+                                           aria-describedby="sizing-addon2" value="{{$user[0]->name??old('name')}}">
                                 </div>
+                            </div>
 
-                                <p>Name : {{$user[0]->name}}</p>
-                                <p>Email : {{$user[0]->email}}</p>
-                                <p>User since : {{$user[0]->created_at}}</p>
-                                <p>Last updated on : {{$user[0]->updated_at}}</p>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="sizing-addon2">DOB</span>
+                                    <input type="text" class="form-control" placeholder=""
+                                           aria-describedby="sizing-addon2">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="sizing-addon2">Gender</span>
+                                    <input type="text" class="form-control" placeholder=""
+                                           aria-describedby="sizing-addon2">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon" id="sizing-addon2">Position</span>
+                                    <input type="text" class="form-control" placeholder=""
+                                           aria-describedby="sizing-addon2">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <p>Profile image</p>
+                                <div class="row">
+
+                                    <div class="col-xs-4 col-md-3">
+                                        <a href="#" class="thumbnail">
+                                            <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZlOGY3MTdjYiB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmU4ZjcxN2NiIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxMy4xNzk2ODc1IiB5PSIzNi41Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg=="
+                                                 alt="...">
+                                        </a>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="file" name="profileImage" id="fileToUpload">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <input type="submit"  class="btn btn-primary" value="Update" name="submit">
                             </div>
 
 
+                        </form>
                     </div>
                 </div>
             </div>
