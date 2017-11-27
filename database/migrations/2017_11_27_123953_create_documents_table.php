2256aa5da97454bd0bf5053fd7fab247250ebc34
Gopal Sharma <gopalindians@gmail.com>
2017-11-27 19:54:12 +0530

    Added documents table migration

---
+++ database/migrations/2017_11_27_123953_create_documents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->describe('Primary key of Project table');
+            $table->integer('creator_id')->describe('Primary key of the user who created the document');
+            $table->text('document_title')->describe('Title of the document');
+            $table->mediumText('document_body')->describe('Body of the document');
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
