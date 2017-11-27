7c18ff1942d42034de0b4466b38cbd0ca73515d8
Gopal Sharma <gopalindians@gmail.com>
2017-11-27 11:21:29 +0530

    Added routes for api

--- routes/web.php
+++ routes/web.php
@@ -13,29 +13,48 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/you', 'ProfileController@index')->name('you');
+Route::get( '/', 'HomeController@index' )->name( 'home' );
+Route::get( '/home', 'HomeController@index' )->name( 'home' );
+Route::get( '/you', 'ProfileController@index' )->name( 'you' );
 
-Route::prefix('profile')->group(function () {
-    Route::get('/edit/{id}', 'ProfileController@editProfile');
-    Route::post('/edit/{id}', 'ProfileController@postEditProfile');
-    Route::get('/{id}/{name?}', 'ProfileController@getProfile');
-});
-
-
+Route::prefix( 'profile' )->group( function () {
+	Route::get( '/edit/{id}', 'ProfileController@editProfile' );
+	Route::post( '/edit/{id}', 'ProfileController@postEditProfile' );
+	Route::get( '/{id}/{name?}', 'ProfileController@getProfile' );
+} );
 
 
-Route::get('/team', 'TeamController@index')->name('team');
-Route::get('/team/add', 'TeamController@addNew');
-Route::post('/team/add', 'TeamController@postAddNew');
-Route::post('/team/join', 'TeamController@postJoin');
-Route::get('/team/join_prompt', 'TeamController@getJoinPrompt');
+Route::get( '/team', 'TeamController@index' )->name( 'team' );
+Route::get( '/team/add', 'TeamController@addNew' );
+Route::post( '/team/add', 'TeamController@postAddNew' );
+Route::post( '/team/join', 'TeamController@postJoin' );
+Route::get( '/team/join_prompt', 'TeamController@getJoinPrompt' );
 
-Route::get('/projects', 'ProjectController@index')->name('projects');
-Route::get('/createProject', 'ProjectController@createProject');
-Route::post('/createProject', 'ProjectController@postCreateProject');
+Route::get( '/projects', 'ProjectController@index' )->name( 'projects' );
+Route::get( '/api/projects', 'ProjectController@apiGetProjects' );
+Route::get( '/api/project/{id}/{projectName?}', 'ProjectController@apiGetProject' );
 
-Route::get('/project/{id}/{name?}/todolists', 'TodoController@index');
-Route::get('/project/{id}/{name?}', 'ProjectController@view');
+Route::get( '/createProject', 'ProjectController@createProject' );
+Route::post( '/createProject', 'ProjectController@postCreateProject' );
+
+//Todolists
+Route::get( '/project/{id}/{name?}/todolists', 'TodoController@index' );
+Route::get( '/project/{id}/{name?}', 'ProjectController@view' );
+
+
+Route::get( '/project/{id}/{name?}/topics', 'TopicController@index' );
+Route::get( '/api/project/{id}/{name?}/topics', 'TopicController@apiGetTopics' );
+Route::get( '/project/{id}/{name?}/messages/new', 'MessageController@index' );
+
+Route::get( '/project/{id}/{name?}/messages/{topicId}', 'MessageController@getTopic' );
+Route::get( '/api/project/{id}/{name?}/messages/{topicId}', 'MessageController@apiGetTopic' );
+
+Route::get( '/project/{id}/{name?}/messages/{topicId}/edit', 'MessageController@getEditTopic' );
+Route::post( '/project/{id}/{name?}/messages/{topicId}/edit', 'MessageController@postEditTopic' );
+
+Route::post( '/project/{id}/{name?}/messages', 'MessageController@postNew' );
+
+
+
+
 
