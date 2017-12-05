094cccec535646408d2a3962cfd40fc55488f85f
Gopal Sharma <gopalindians@gmail.com>
2017-12-05 19:29:06 +0530

    Added new css in layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="home-page" content="{{env('APP_URL') }}">
+    <meta name="sub-url" content="{{env('SUB_URL') }}">
     <title>@yield('title') - {{ config('', 'LeadCamp') }}</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
     <!-- new file -->
 {{--    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet"/>--}}
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
