b4ef515f053ec301f48991952065890db36f26c7
Gopal Sharma <gopalindians@gmail.com>
2017-12-07 18:59:30 +0530

    Updated Controller for getPeople method

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class PeopleController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function getPeople(Request $request)
+    {
+        $peopleId = $request->route('people_id');
+
+        $result = DB::table('users')->where(
+            'id', $peopleId
+        )->get();
+        return view('people.index', ['user' => $result]);
+
+    }
 }
