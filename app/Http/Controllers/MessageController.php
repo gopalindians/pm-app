690c017e4dda852383899c82b890cf1f2149f322
Gopal Sharma <gopalindians@gmail.com>
2017-12-14 18:45:54 +0530

    Added method to add Comment to Messages

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\CH;
+
 use Auth, DB;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
@@ -76,7 +76,7 @@ class MessageController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function apiGetTopic(Request $request): JsonResponse
+    public function apiGetTopic(Request $request)
     {
 
         $topicId = $request->route('topicId');
@@ -164,5 +164,101 @@ class MessageController extends Controller
 
 
         return redirect('project/' . $projectId . '/' . $projectName . '/messages/' . $topicId);
+    }
+
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function apiPostTopicComment(Request $request)
+    {
+        $posterId = $request->post('posterId');
+        $topicComment = $request->post('topicComment');
+        $projectId = $request->route('id');
+        $topicId = $request->route('topicId');
+
+
+        try {
+            $topicCommentId = DB::table('topic_comments')
+                ->insertGetId([
+                    'project_id' => $projectId,
+                    'topic_id' => $topicId,
+                    'poster_id' => $posterId,
+                    'comment' => $topicComment,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]);
+
+            DB::table('projects')
+                ->where('id', $projectId)
+                ->update([
+                    'updated_at' => Carbon::now()
+                ]);
+
+
+            $topic = DB::table('topics')
+                ->select(
+                    'topics.id as topic_id',
+                    'topics.topic_name',
+                    'topics.topic_body',
+                    'topics.created_at as topic_created_at',
+                    'topics.updated_at as topic_updated_at',
+
+                    'users.id as creater_id',
+                    'users.name as creater_name',
+                    'users.email as creater_email',
+                    'users.profile_image as creater_profile_image',
+
+                    'projects.id as project_id',
+                    'projects.owner_id as project_creater_id',
+                    'projects.project_name',
+                    'projects.project_description',
+                    'projects.state as project_state'
+                )
+                ->join('users', 'users.id', 'topics.creater_id')
+                ->join('projects', 'projects.id', 'topics.project_id')
+                ->where('topics.id', $topicId)
+                ->where('topics.project_id', $projectId)
+                ->where('topics.project_id', $projectId)
+                ->get();
+
+            foreach ($topic as $item) {
+
+                $topicComments = DB::table('topic_comments')
+                    ->where('topic_comments.id', $topicCommentId)
+                    ->get();
+                foreach ($topicComments as $tc) {
+                    $tc->comment_poster = DB::table('users')
+                        ->where('users.id', $tc->poster_id)
+                        ->first();
+                    unset($tc->comment_poster->password, $tc->comment_poster->remember_token);
+
+                    $tc->created_at_human = str_replace('before', 'ago', Carbon::parse($tc->created_at)->diffForHumans(Carbon::now()));
+                    $tc->created_at_noob = Carbon::parse($tc->created_at)->format('l jS \\of F Y h:i:s A');
+
+                    $tc->updated_at_human = str_replace('before', 'ago', Carbon::parse($tc->updated_at)->diffForHumans(Carbon::now()));
+                    $tc->updated_at_noob = Carbon::parse($tc->updated_at)->format('l jS \\of F Y h:i:s A');
+
+                }
+                $item->topic_comments = $topicComments;
+                $item->topic_created_at_human = str_replace('before', 'ago', Carbon::parse($item->topic_created_at)->diffForHumans(Carbon::now()));
+                $item->topic_created_at_noob = Carbon::parse($item->topic_created_at)->format('l jS \\of F Y h:i:s A');
+                $item->topic_updated_at_human = str_replace('before', 'ago', Carbon::parse($item->topic_updated_at)->diffForHumans(Carbon::now()));
+                $item->topic_updated_at_noob = Carbon::parse($item->topic_updated_at)->format('l jS \\of F Y h:i:s A');
+            }
+
+
+        } catch (\Exception $exception) {
+            $response['message'] = 'Error saving message';
+            $response['type'] = 'error';
+            return response()->json($response);
+        }
+        $response['message'] = 'Message posted successfully';
+        $response['type'] = 'success';
+        $response['data'] =$topic[0];
+        return response()->json($response);
+
+
     }
 }
