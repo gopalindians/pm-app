b41ad9ebed4e95f4f14ef2ad7c9eb4a2cf276fa5
Gopal Sharma <gopalindians@gmail.com>
2017-12-17 23:52:59 +0530

    Updated ExampleTest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
 
-        $this->get('team/add');
+        $this->get('/');
         $this->assertTrue(true);
     }
 }
