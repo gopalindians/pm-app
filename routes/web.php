5ee98fc15fe84ccff19730d375110493ccb6c359
Gopal Sharma <gopalindians@gmail.com>
2017-12-17 22:09:30 +0530

    Added new routes

--- routes/web.php
+++ routes/web.php
@@ -13,95 +13,86 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get( '/', 'HomeController@index' )->name( 'home' );
+Route::get( '/home', 'HomeController@index' )->name( 'home' );
 
 
-Route::prefix('profile')->group(function () {
-    Route::get('/edit/{id}', 'ProfileController@editProfile');
-    Route::post('/edit/{id}', 'ProfileController@postEditProfile');
-    Route::get('/{id}/{name?}', 'ProfileController@getProfile');
-});
+Route::prefix( 'profile' )->group( function () {
+	Route::get( '/edit/{id}', 'ProfileController@editProfile' );
+	Route::post( '/edit/{id}', 'ProfileController@postEditProfile' );
+	Route::get( '/{id}/{name?}', 'ProfileController@getProfile' );
+} );
 
 
-Route::get('/team', 'TeamController@index')->name('team');
-Route::get('/team/add', 'TeamController@addNew');
-Route::post('/team/add', 'TeamController@postAddNew');
-Route::post('/team/join', 'TeamController@postJoin');
-Route::get('/team/join_prompt', 'TeamController@getJoinPrompt');
+Route::get( '/team', 'TeamController@index' )->name( 'team' );
+Route::get( '/team/add', 'TeamController@addNew' );
+Route::post( '/team/add', 'TeamController@postAddNew' );
+Route::post( '/team/join', 'TeamController@postJoin' );
+Route::get( '/team/join_prompt', 'TeamController@getJoinPrompt' );
 
-Route::get('/projects', 'ProjectController@index')->name('projects');
-//Route::get('/api/projects', 'ProjectController@apiGetProjects');
-//Route::get('/api/project/{id}/{projectName?}', 'ProjectController@apiGetProject');
+Route::get( '/projects', 'ProjectController@index' )->name( 'projects' );
 
-Route::get('/createProject', 'ProjectController@createProject');
-//Route::post('/api/createProject', 'ProjectController@postCreateProject');
+Route::get( '/createProject', 'ProjectController@createProject' );
+
 
 //Todolists
-Route::get('/project/{id}/{name?}/todolists', 'TodoController@index');
-Route::get('/project/{id}/{name?}', 'ProjectController@view');
+Route::get( '/project/{id}/{name?}/todolists', 'TodoController@index' );
+Route::get( '/project/{id}/{name?}', 'ProjectController@view' );
 
 
-Route::get('/project/{id}/{name?}/topics', 'TopicController@index');
-//Route::get('/api/project/{id}/{name?}/topics', 'TopicController@apiGetTopics');
+Route::get( '/project/{id}/{name?}/topics', 'TopicController@index' );
+Route::get( '/api/project/{id}/{name?}/document/{documentId}', 'DocumentController@apiGetDocument' );
 
+Route::prefix( '/project/{id}/{name?}' )->group( function () {
+	//messages
+	Route::prefix( '/messages' )->group( function () {
+		Route::get( '/new', 'MessageController@index' );
+		Route::get( '/{topicId}', 'MessageController@getTopic' );
+		Route::post( '/{topicId}/edit', 'MessageController@postEditTopic' );
+		Route::get( '/{topicId}/edit', 'MessageController@getEditTopic' );
+		Route::get( '/{topicId}/trash', 'MessageController@deleteMessage' );
+		Route::post( '/', 'MessageController@postNew' );
+	} );
 
-//Route::get('/project/{id}/{name?}/messages/new', 'MessageController@index');
+	//documents
+	Route::prefix( '/documents' )->group( function () {
+		Route::get( '/', 'DocumentController@getDocuments' );
+		Route::get( '/new', 'DocumentController@getNewDocumentView' );
+		Route::post( '/', 'DocumentController@postNewDocument' );
+		Route::get( '/{documentId}', 'DocumentController@getDocument' );
+	} );
 
-//Route::get('/project/{id}/{name?}/messages/{topicId}', 'MessageController@getTopic');
-///
-//Route::get('/api/project/{id}/{name?}/messages/{topicId}', 'MessageController@apiGetTopic');
-
-//Route::get('/project/{id}/{name?}/messages/{topicId}/edit', 'MessageController@getEditTopic');
-//Route::post('/project/{id}/{name?}/messages/{topicId}/edit', 'MessageController@postEditTopic');
-
-//Route::post('/project/{id}/{name?}/messages', 'MessageController@postNew');
-
-
-//Route::get('/project/{id}/{name?}/documents', 'DocumentController@getDocuments');
-//Route::get('/api/project/{id}/{name?}/documents', 'DocumentController@apiGetDocuments');
-//Route::get('/project/{id}/{name?}/document/new', 'DocumentController@getNewDocumentView');
-//Route::post('/project/{id}/{name?}/document', 'DocumentController@postNewDocument');
-//Route::get('/project/{id}/{name?}/document/{documentId}', 'DocumentController@getDocument');
-Route::get('/api/project/{id}/{name?}/document/{documentId}', 'DocumentController@apiGetDocument');
-
-Route::prefix('/project/{id}/{name?}')->group(function () {
-
-    //messages
-    Route::get('/messages/new', 'MessageController@index');
-    Route::get('/messages/{topicId}', 'MessageController@getTopic');
-    Route::post('/messages/{topicId}/edit', 'MessageController@postEditTopic');
-    Route::get('/messages/{topicId}/edit', 'MessageController@getEditTopic');
-
-    Route::post('/messages', 'MessageController@postNew');
-
-    //documents
-    Route::get('/documents', 'DocumentController@getDocuments');
-    Route::get('/document/new', 'DocumentController@getNewDocumentView');
-    Route::post('/document', 'DocumentController@postNewDocument');
-    Route::get('/document/{documentId}', 'DocumentController@getDocument');
-});
+} );
 
 
 //Search
-Route::get('/search', 'SearchController@getSearch');
-Route::get('/api/search', 'SearchController@getSearchQuery');
+Route::get( '/search', 'SearchController@getSearch' );
+
 
 //people
+Route::get( '/people/{people_id}', 'PeopleController@index' );
 
-Route::get('/people/{people_id}', 'PeopleController@index');
-Route::get('/api/people/{people_id}', 'PeopleController@getPeople');
 
-Route::prefix('/api')->group(function () {
+Route::prefix( '/api' )->group( function () {
 
-    Route::get('/projects', 'ProjectController@apiGetProjects');
-    Route::get('/project/{id}/{projectName?}', 'ProjectController@apiGetProject');
-    Route::post('/createProject', 'ProjectController@postCreateProject');
-    Route::get('/project/{id}/{name?}/topics', 'TopicController@apiGetTopics');
-    Route::get('/project/{id}/{name?}/messages/{topicId}', 'MessageController@apiGetTopic');
-    Route::get('/project/{id}/{name?}/documents', 'DocumentController@apiGetDocuments');
-    Route::get('/project/{id}/{name?}/messages/{topicId}', 'MessageController@apiGetTopic');
-});
+	Route::get( '/search', 'SearchController@getSearchQuery' );
+
+	Route::get( '/people/{people_id}', 'PeopleController@getPeople' );
+
+	Route::get( '/projects', 'ProjectController@apiGetProjects' );
+	Route::post( '/createProject', 'ProjectController@postCreateProject' );
+
+
+	Route::prefix( '/project/{id}' )->group( function () {
+		Route::get( '/{projectName?}', 'ProjectController@apiGetProject' );
+		Route::get( '/{name?}/topics', 'TopicController@apiGetTopics' );
+		Route::get( '/{name?}/messages/{topicId}', 'MessageController@apiGetTopic' );
+		Route::get( '/{name?}/documents', 'DocumentController@apiGetDocuments' );
+		Route::get( '/{name?}/messages/{topicId}', 'MessageController@apiGetTopic' );
+	} );
+
+
+} );
 
 
 
